671001110de3ab43835aadfb6d9d60bde6e374a7
Eris1allkanjari <eris.allkanjari@gmail.com>
2021-07-20 17:07:59 +0200

    working with blobs

--- libwyag.py
+++ libwyag.py
@@ -222,3 +222,61 @@ class GitObject (object):
                 #compress and write
                 f.write(zlib.compress(result))
         return sha             
+
+class GitBlob(GitObject):
+    fmt= b'blob'
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data   
+
+    argsp = argsubparsers.add_parser("cat-file",help="Provide content of repository objects") 
+
+    argsp.add_argument("type",
+                        metavar="type",
+                        choices=["blob","commit","tag","tree"],
+                        help="Specify a type")  
+
+    argsp.add_argument("object",metavar="object",help="The object to display")   
+
+    def cmd_cat_file(args):
+        repo = repo_find()
+        cat_file(repo,args.object,fmt = args.type.encode())
+
+    def cat_file(repo,obj,fmt=None):
+        obj = object_read(repo,object_find(repo,obj,fmt=fmt))
+        sys.stdout.buffer.write(obj.serialize)    
+
+
+    argsp = argsubparsers.add_parser("hash-object",help="Compute object ID and optionally creates a blob from a file") 
+
+    argsp.add_argument("-t",metavar="type",dest="type",choices=["blob","commit","tag","tree"],default="blob",help="Specify the type")   
+
+    argsp.add_argument("-w",dest="write",action="store_true",help="Actually write the object into the database")
+
+    argsp.add_argument("path",help="Read object from <file>")
+
+    def cmd_hash_object(args):
+        if args.write:
+            repo = GitRepository(".")
+        else:
+            repo = None 
+
+        with open(args.path,"rb") as fd:
+            sha = object_hash(fd,args.type.encode(),repo)
+            print(sha)
+
+    def object_hash(fd,fmt,repo=None):
+        data = fd.read()
+
+        #choose constructore depending on object type found on header
+        if   fmt==b'commit' : obj=GitCommit(repo, data)
+        elif fmt==b'tree'   : obj=GitTree(repo, data)
+        elif fmt==b'tag'    : obj=GitTag(repo, data)
+        elif fmt==b'blob'   : obj=GitBlob(repo, data)
+        else:
+         raise Exception("Unknown type %s!" % fmt)
+
+        return object_write(obj, repo)                               
